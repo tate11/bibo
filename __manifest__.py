52bee7211bdd74413e6022d65589dad87502cefc
Nay3li <nayeli.valencia@xmarts.com.mx>
2017-07-31 16:38:20 -0500

    Personalizaciones de BIBO

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Bibo',
+    'version': '10.0.0',
+    'summary': 'Personalizacion de bibo',
+    'description': 'Modificacion de la orden de compra',
+    'category': 'Personalizaicion',
+    'author': 'Raul Ovalle, Nayeli Valencia Díaz',
+    'website': 'www.xmarts.com',
+    'depends': [
+        'purchase',
+        'purchase_discount',
+                ],
+    'data': [
+        'reports/purchase_order_report.xml',
+        'reports/invoice_report.xml',
+        'views/product_template_view.xml',
+
+    ],
+    'installable': True,
+    'auto_install': False,
+}
