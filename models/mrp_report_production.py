67ce3784b60e140aa5ef48054b8d209127c113aa
Nay3li <nayeli.valencia@xmarts.com.mx>
2018-01-24 17:36:35 -0600

    estatus pagado no poder editar

--- models/mrp_report_production.py
+++ models/mrp_report_production.py
@@ -12,7 +12,7 @@ class MrpReportProduction(models.Model):
             mrp.subtotal = total
 
     name = fields.Many2one('hr.employee', string="Empleado", readonly=True)
-    production_lines = fields.One2many('mrp.report.production.line', 'production_id', string='Table lines')
+    production_lines = fields.One2many('mrp.report.production.line', 'production_id', string='Table lines',readonly=False,states={'pay': [('readonly', True)]})
     date_start = fields.Date('Fecha Inicio',readonly=True)
     date_finish = fields.Date('Fecha Fin', readonly=True)
     subtotal = fields.Float('Subtotal', compute="_compute_suma")
@@ -58,5 +58,5 @@ class MrpReportProductionLine(models.Model):
     mrp_production_id = fields.Many2one('mrp.production', string="Orden de Produccion", readonly=False)
     operation_id = fields.Many2one('mrp.workorder', string="Operacion", readonly=False)
     qty= fields.Integer('Piezas', readonly=False)
-    precio_unit = fields.Float('Precio', readonly=False,states={'pay': [('readonly', True)]})
+    precio_unit = fields.Float('Precio', readonly=False)
     total = fields.Float('Total', readonly=False)
